afbb75ead7dcf5c09e929b6731ef52f82794d20f
edtheories <68911247+edtheories@users.noreply.github.com>
2020-07-28 21:14:29 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -36,16 +36,17 @@ function genQuote() {
   var randNum2 = Math.floor(Math.random() * 8) + 1;
   var randNum3 = Math.floor(Math.random() * 7) + 1;
   var randNum4 = Math.floor(Math.random() * 9) + 1;
-  document.getElementById('quote').innerHTML = quotes1[randNum1]+quotes2[randNum2]+quotes3[randNum3]+quotes2a[randNum2]+quotes4[randNum4];
+  document.getElementById('quote').innerHTML = quotes1[randNum1]+quotes2[randNum2]+quotes3[randNum3]+quotes2a[randNum2]+quotes1a[randNum1]+quotes4[randNum4];
 }
 
 //quote array
-var quotes1 = ["Blank", "The newest episode of PST is about how Ed used to ", "After 18 months of not uploading, Ed finally releases a video titled <br/> How to ", "Ed's stand ability is to instantly ", "The plot of MGR2 is that Raiden has to "]
+var quotes1 = ["Blank", "The newest episode of PST is about how Ed used to ", "After 18 months of not uploading, Ed finally releases a video titled <br/> How to '", "Ed's stand ability is to instantly ", "The plot of MGR2 is that Raiden has to "]
 console.log(quotes1)
 var quotes2 = ["Blank","watch Face/Off with ","get ","start a ","get ","get ","use ","get half-cirumcised by ","gobble "]
 var quotes3 = ["Blank","Ed's ex","David","An amputee","Smite Car 3","DoubleJ","Kuzenbo","Hbomberguy"]
 var quotes2a = ["Blank"," "," to shit in a Nutella jar "," themed pedophile ring "," addicted to crack "," tattooed on his thigh "," as a catheter "," ","'s toes "]
 var quotes4 = ["Blank","<br/>and that's how he woke up in France.","<br/>and then he gets banished to the punta cana airport for eternity.","<br/>and that's why Xbalanque can't read.","at the ScreenWave media booth.","<br/>and thats why David hates bread.","with Viper the Rapper.","at the Punta Cana airport.","<br/>and that's why Ed hates cheese.","in a fursuit."]
+var quotes1a = ["Blank","","'","",""]
 
 //date
 function startDate() {
